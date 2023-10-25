ea0ac3d8544ed4f35c31988f28e28618b09548e2
Oleksandra <sashasymko122@gmail.com>
2023-10-26 00:17:55 +0300

    Forth commit

--- src/main/java/ua/edu/ucu/apps/flowerstore/Items/Order.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/Items/Order.java
@@ -5,27 +5,27 @@ import ua.edu.ucu.apps.flowerstore.payment.Payment;
 
 import java.util.ArrayList;
 public class Order {
-    private Payment wayofpayment;
-    private Delivery wayofdelivery;
+    private Payment payment;
+    private Delivery delivery;
 
     private final ArrayList<Item> myLst;
 
     public Order() {
         myLst = new ArrayList<>();
     }
-    public void setPaymentMethod(Payment wayofpayment) {
-        this.wayofpayment = wayofpayment;
+    public void setPaymentMethod(Payment chosenPayment) {
+        this.payment = chosenPayment;
     }
-    public void setDeliverStrategy(Delivery wayofdelivery) {
-        this.wayofdelivery = wayofdelivery;
+    public void setDeliverStrategy(Delivery chosenDelivery) {
+        this.delivery = chosenDelivery;
     }
-    public double countPrice() {
+    public double CountPrice() {
         return myLst.stream().mapToDouble(Item::price).sum();
     }
     public void processingOrder() {
-        if (wayofpayment.pay(countPrice())) {
+        if (payment.pay(CountPrice())) {
             System.out.println("Payment is done");
-            wayofdelivery.delivery();
+            delivery.delivery();
         }
     }
     public void addItem(Item item) {
