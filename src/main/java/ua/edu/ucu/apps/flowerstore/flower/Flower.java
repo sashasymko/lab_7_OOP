ff5b1ffdf7acf05c44fdc1b0d4e737fb17cba944
sashasymko <116544323+sashasymko@users.noreply.github.com>
2023-11-05 21:35:42 +0200

    Update Flower.java

--- src/main/java/ua/edu/ucu/apps/flowerstore/flower/Flower.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/flower/Flower.java
@@ -1,17 +1,38 @@
 package ua.edu.ucu.apps.flowerstore.flower;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ua.edu.ucu.apps.flowerstore.items.Item;
+import ua.edu.ucu.apps.flowerstore.properties.FlowerColor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Getter
-@AllArgsConstructor
-@Entity
-@NoArgsConstructor
-public class Flower {
-    @Id
-    private double price;
+
+@Getter @Setter @AllArgsConstructor @Entity @Table @NoArgsConstructor
+public class Flower extends Item {
+    @GeneratedValue @Id
+    private int id;
     private double sepalLength;
-    private FlowerColor color;
+    private String color;
+    protected double price;
+
+    public Flower(FlowerColor color, double sepalLength, double price) {
+        setColor(color);
+        this.sepalLength = sepalLength;
+        this.price = price;
+    }
+
+    private void setColor(FlowerColor color) {
+        this.color = color.toString();
+    }
+
+    @Override
+    public double price() {
+        return price;
+    }
+
+
 }
