2251cc4ffa7c1757c6ede8ae11539055384a433a
sashasymko <116544323+sashasymko@users.noreply.github.com>
2023-11-05 21:19:42 +0200

    Update FlowerService.java

--- src/main/java/ua/edu/ucu/apps/flowerstore/controller/FlowerService.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/controller/FlowerService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FlowerService {
-    public FlowerRepository flowerRepository;
+    private FlowerRepository flowerRepository;
 
     @Autowired
     public FlowerService(FlowerRepository flowerRepository) {
